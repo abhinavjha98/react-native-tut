0009985ae6e3d52637e163a1641f1fa4f1f47f28
Abhinav <abhinavjha98ald@gmail.com>
2019-04-28 15:53:29 +0530

    Added the title

--- albums/index.js
+++ albums/index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+// Import a library to help a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+//Create a component JSX
+const App = () => (
+		<Text>Some Text</Text>
+	);
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+//Render it to device
+AppRegistry.registerComponent('albums', () => App);
