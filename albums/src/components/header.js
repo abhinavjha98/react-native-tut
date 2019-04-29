d963f5000ca6bdc1f680c5661e11fa488ba5d2cb
Abhinav <abhinavjha98ald@gmail.com>
2019-04-29 22:49:22 +0530

    Added the title in header

---
+++ albums/src/components/header.js
@@ -0,0 +1,24 @@
+//import libraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//make a component
+const Header = () => {
+	const { textStyle, viewStyle } = styles;
+	return (
+		<View style={viewStyle}>
+			<Text style = {textStyle}>Albums!</Text>;
+		</View>
+	);
+};
+
+const styles = {
+	viewStyle:{
+		backgroundColor:'#F8F8F8'
+	},
+	textStyle :{
+		fontSize : 20
+	}
+};
+//make the component available for other pastrs of the app
+export default Header;
