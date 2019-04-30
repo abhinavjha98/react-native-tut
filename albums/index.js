95731468ec36595f17ea72e4cb54605d80cc54d2
Abhinav <abhinavjha98ald@gmail.com>
2019-04-30 13:58:45 +0530

    Added http requests

--- albums/index.js
+++ albums/index.js
@@ -1,12 +1,16 @@
 // Import a library to help a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-
+import AlbumList from './src/components/AlbumList';
 //Create a component JSX
-const App = () => (
-	<Header />
+const App = () => {
+	return (
+	<View>
+		<Header headerText={'Albums! '} />
+		<AlbumList />
+		</View>
 	);
-
+};
 //Render it to device
 AppRegistry.registerComponent('albums', () => App);
